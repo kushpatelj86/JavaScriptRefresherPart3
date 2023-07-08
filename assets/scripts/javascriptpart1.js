ce48fd4634946addea75eaf45aa02f854009220a
Kush Patel <111901629+kushpatelj86@users.noreply.github.com>
2023-07-08 10:29:05 -0700

    Initial commit

---
+++ assets/scripts/javascriptpart1.js
@@ -0,0 +1,12 @@
+//import { apiKey } from "./utlities.js";
+
+//import apiKey from "./utlities.js";
+
+//import { apiKey, abc } from "./utlities.js";
+
+//import { apiKey, abc as content} from "./utlities.js";
+
+import * as util from "./util";
+//console.log(apiKey);
+
+console.log(util.apiKey);
---
+++ assets/scripts/util.js
@@ -0,0 +1,3 @@
+export let apiKey = "jhgfds76543wedfgho87";
+//export default "jhgfds76543wedfgho87";
+export let abc = "gfds";
